b33884540966c6286efe5c2d241cca272f324f62
raju2806 <raju7654030601@gmail.com>
2024-09-23 15:55:57 +0530

    first commit

---
+++ views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+def home(request):
+    return render(request,'home.html')
+
+def predict(request):
+    return render(request,'predict.html')
+
+def result(request):
+    data = pd.read_csv(diabetes\diabetespima.csv)
+    x = data.drop("Outcome",axis=1)
+    y=data['Outcome']
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
+    model=SVC(kernel='linear',random_state=0)
+    model.fit(x_train,y_train)
+
+    val1= float(request.GET['n1'])
+    val2= float(request.GET['n2'])
+    val3= float(request.GET['n3'])
+    val4= float(request.GET['n4'])
+    val5= float(request.GET['n5'])
+    val6= float(request.GET['n6'])
+    val7= float(request.GET['n7'])
+    val8= float(request.GET['n8'])
+    
+    pred=model.predict([[val1,val2,val3,val4,val5,val6,val7,val8]])
+
+    result1=""
+    if pred==[1]:
+        result1= "Positive"
+    else:
+        result1= "Negative"
+
+
+    return render(request,'predict.html',{"result2":result1})
